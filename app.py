b2045144f3e6ec4ea2022a8b0a39b3ac9084cee6
irfan <Irfan00roshan@gmail.com>
2022-02-20 16:25:29 +0530

    addeded fancy image to response

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 import pandas as pd
@@ -10,13 +11,26 @@ class Dataset:
 
     def __init__(self):
         self.symptoms = []
+        self.diseases = []
+        self.wiki = {}
         self.load_dataset()
 
     def load_dataset(self):
         self.load_symptoms()
+        self.load_diseases()
+        self.load_wiki()
+
+    def load_wiki(self):
+        fldr = os.path.join("Data", "Wiki")
+        with open(os.path.join(fldr, "diseases.json"), "r", encoding="UTF-8") as fp:
+            self.wiki = json.load(fp)
+
+    def load_diseases(self):
+        with open(os.path.join("Data", "diseases_norm.txt"), "r", encoding="UTF-8") as fp:
+            self.diseases = fp.read().splitlines()
 
     def load_symptoms(self):
-        with open(os.path.join("Data", "symptoms.txt"), "r") as fp:
+        with open(os.path.join("Data", "symptoms.txt"), "r", encoding="UTF-8") as fp:
             self.symptoms = fp.read().splitlines()
 
     def get_user_response(self, user_symptoms):
@@ -27,9 +41,27 @@ class Dataset:
         return pd.DataFrame(symptoms_vector).transpose()
 
 
-def display_result(response):
+def display_result(response, dataset):
+    fldr = os.path.join("Data", "Wiki", "imgs")
     with use_scope("result", clear=True):
-        put_text("Congrats you have " + response[0])
+        put_text("Your symptoms best match " + response[0])
+
+        wiki_info = dataset.wiki[response[0]]
+        heading = response[0]
+        if "__HEADING__" in wiki_info.keys():
+            heading = wiki_info["__HEADING__"]
+        info = []
+        for key in wiki_info.keys():
+            if key[:2] != "__" and key[-2:] != "__":
+                row = [key, wiki_info[key]]
+                info.append(row)
+        if "__IMAGE_FILE__" in wiki_info.keys():
+            img = open(os.path.join(fldr, wiki_info["__IMAGE_FILE__"]), 'rb').read()
+            put_image(img, width='50%')
+        put_table(
+            info,
+            header=[span(heading, col=2)])
+
 
 
 def search(resp, dataset, dt):
@@ -50,7 +82,7 @@ def search(resp, dataset, dt):
         fp.write(str(vector))
     response = dt.get_diseases(vector)
     print(response)
-    display_result(response)
+    display_result(response, dataset)
 
 
 def debug_print(s):
@@ -64,9 +96,11 @@ def main():
     dataset = Dataset()
     put_scope("result", content=put_text(""))
     put_scope("warnings", content=put_text(""))
-    while True:
+    print(" getting diseases")
+    for disease in dataset.diseases:
+        print(" got 1 "+ disease)
         resp = input("enter symptoms separated by comma")
-        search(resp, dataset, dt)
+        display_result([disease], dataset)
 
 
 if __name__ == '__main__':
